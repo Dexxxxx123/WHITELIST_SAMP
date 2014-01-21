f080012cbe794e41ab1af51fd00472db1a8d91d9
Giampaolo Falqui <giampaolofalqui@gmail.com>
2014-01-22 00:58:34 +0100

    Creates layout, controllers and models

---
+++ app/Controller/ApiKeysController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * API keys controller.
+ *
+ * BLACKLIST_SAMP: Global Blacklist Platform (https://github.com/GiampaoloFalqui/BLACKLIST_SAMP)
+ * 
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @link          https://github.com/GiampaoloFalqui/BLACKLIST_SAMP BLACKLIST_SAMP
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+ 
+/**
+ * API keys controller controller.
+ * 
+ * 
+ */
+class ApiKeysController extends AppController {
+  
+}
