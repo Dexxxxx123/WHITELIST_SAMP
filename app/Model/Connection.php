804d384b171f4717335f24e2ebcf5dae54506462
Giampaolo Falqui <giampaolofalqui@gmail.com>
2014-01-23 21:38:12 +0100

    Changes addresses to connections, makes more sense

---
+++ app/Model/Connection.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Connection model.
+ *
+ * WHITELIST_SAMP: Global Whitelist Platform (https://github.com/GiampaoloFalqui/WHITELIST_SAMP)
+ * 
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @link          https://github.com/GiampaoloFalqui/WHITELIST_SAMP WHITELIST_SAMP
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+/**
+ * Connection model.
+ *
+ *
+ */
+class Connection extends Model {
+  
+  /**
+   * An address belongs to an alias.
+   */
+  public $belongsTo = 'Alias';
+}
